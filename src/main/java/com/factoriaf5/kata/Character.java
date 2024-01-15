7c9332664168f3f86c09d9f0440856a15f1f7f82
Brusmel Gil <Brusmel@outlook.es>
2024-01-15 09:53:29 +0100

    Iteration One

--- src/main/java/com/factoriaf5/kata/Character.java
+++ src/main/java/com/factoriaf5/kata/Character.java
@@ -2,4 +2,37 @@ package com.factoriaf5.kata;
 
 public class Character {
     
+    int health = 1000;
+    int level = 1;
+    boolean isAlive = true;
+    int attackDamage = 100;
+
+
+    public int getHealth() {
+        return health;
+    }
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    public int getLevel() {
+        return level;
+    }
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    public boolean isAlive() {
+        return isAlive;
+    }
+    public void setAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+    public int getAttackDamage() {
+        return attackDamage;
+    }
+    public void setAttackDamage(int attackDamage) {
+        this.attackDamage = attackDamage;
+    }
+
+    
 }
+
--- src/test/java/com/factoriaf5/kata/CharacterTest.java
+++ src/test/java/com/factoriaf5/kata/CharacterTest.java
@@ -1,5 +1,7 @@
 package com.factoriaf5.kata;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -7,8 +9,18 @@ import org.junit.jupiter.api.Test;
 public class CharacterTest {
     
     @Test
-    public void ExampleTest() {
-        assertEquals(2, 2);
+    public void characterTestHisAttributes() {
+        Character goku = new Character();
+
+        int gokuHealth = goku.getHealth();
+        int gokuLevel = goku.getLevel();
+        boolean gokuIsAlive = goku.isAlive();
+
+        assertThat(gokuHealth, is(1000));
+        assertThat(gokuLevel, is(1));
+        assertThat(gokuIsAlive, is(true));
+
+
     }
 
 }
